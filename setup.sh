6a03555b2f461a74bce2fc4932d1dd5811cd7f44
Gero Posmyk-Leinemann <gero@gitpod.io>
2025-03-04 10:59:54 +0000

    Install linear and git MCP servers
    Tool: gitpod/catfood.gitpod.cloud

--- setup.sh
+++ setup.sh
@@ -11,15 +11,21 @@ fi
 
 pushd "/home/gitpod/.dotfiles"
 
-
+# bash
 echo "Setting up bash..."
 cat .bash_aliases >> ~/.bash_aliases
 cat .bashrc >> ~/.bashrc
 
 # git
-
 echo "Setting up git..."
 printf "\n[include]\npath = /home/gitpod/.dotfiles/.gitconfig\n" >> ~/.gitconfig
+
+# linear MCP server
+# TODO(gpl): Think about a) referencing a fixed commit hash for the script, and b) a fixed version instead of "latest"
+curl -sL https://raw.githubusercontent.com/geropl/linear-mcp-go/refs/heads/main/scripts/register-cline.sh | bash -s -- $LINEAR_API_KEY $LINEAR_MCP_WRITE_ACCESS || true
+
+# git MCP server
+curl -sL https://raw.githubusercontent.com/geropl/git-mcp-go/refs/heads/main/scripts/register-cline.sh | bash -s -- $GITPOD_REPO_ROOT $GIT_MCP_WRITE_ACCESS || true
 
 popd
 
