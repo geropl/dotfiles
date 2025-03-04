7d77c92e9c18ed316d3a069e38daac8dd80daedb
Gero Posmyk-Leinemann <gero@gitpod.io>
2025-03-04 14:01:58 +0000

    update to "linear-mcp-go setup"
    Tool: gitpod/catfood.gitpod.cloud

--- setup.sh
+++ setup.sh
@@ -22,7 +22,13 @@ printf "\n[include]\npath = /home/gitpod/.dotfiles/.gitconfig\n" >> ~/.gitconfig
 
 # linear MCP server
 # TODO(gpl): Think about a) referencing a fixed commit hash for the script, and b) a fixed version instead of "latest"
-curl -sL https://raw.githubusercontent.com/geropl/linear-mcp-go/refs/heads/main/scripts/register-cline.sh | bash -s -- $LINEAR_API_KEY $LINEAR_MCP_WRITE_ACCESS || true
+RELEASE=$(curl -s https://api.github.com/repos/geropl/linear-mcp-go/releases/latest)
+DOWNLOAD_URL=$(echo $RELEASE | jq -r '.assets[] | select(.name | contains("linux")) | .browser_download_url')
+curl -L -o ./linear-mcp-go $DOWNLOAD_URL
+chmod +x ./linear-mcp-go
+
+./linear-mcp-go setup --write-access $LINEAR_MCP_WRITE_ACCESS --auto-approve=allow-read-only || true
+rm -f ./linear-mcp-go
 
 # git MCP server
 curl -sL https://raw.githubusercontent.com/geropl/git-mcp-go/refs/heads/main/scripts/register-cline.sh | bash -s -- $GITPOD_REPO_ROOT $GIT_MCP_WRITE_ACCESS || true
